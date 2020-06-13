1a9652df41bf3f5eb6dabb7dbaac9111b9b7634c
Lavneet <elavneet>
2020-06-13 20:06:00 +0530

    first commit

---
+++ src/Launcher.java
@@ -0,0 +1,8 @@
+public class Launcher {
+
+    public static void main(String[] args) {
+        LabelWriter labelWriter = new LabelWriter();
+        System.out.println("Will print: " + args[0]);
+        labelWriter.printLabel(args[0], args[0], args[0], args[0], args[0], args[0], args[0], args[0], args[0],args[0], args[0], args[0]);
+    }
+}
